5f31cc8a79b897d2aaf5fa402e82bb122192003b
Yong Yang <yangoliver@gmail.com>
2016-10-10 22:05:52 +0800

    Fibonacci generator
    Signed-off-by: Yong Yang <yangoliver@gmail.com>

---
+++ fibonacci/fibonacci.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func fibonacci() func() int {
+	o, p := -1, 1
+
+	return func() int {
+		o, p = p, o+p
+		return p
+	}
+}
+
+func main() {
+	f := fibonacci()
+	for i := 0; i < 10; i++ {
+		fmt.Println(f())
+	}
+}
