5f15a16c3667bf92bf6b6d483d87c266d14173d8
Yong Yang <yangoliver@gmail.com>
2016-10-12 12:06:42 +0800

    reader method example
    Signed-off-by: Yong Yang <yangoliver@gmail.com>

---
+++ reader/reader.go
@@ -0,0 +1,20 @@
+package main
+
+import "golang.org/x/tour/reader"
+
+type MyReader struct{}
+
+// TODO: Add a Read([]byte) (int, error) method to MyReader.
+func (rd MyReader) Read(b []byte) (int, error) {
+	len := 20
+
+	for i := 0; i < len; i++ {
+		b[i] = 'A'
+	}
+
+	return len, nil
+}
+
+func main() {
+	reader.Validate(MyReader{})
+}
