dd666a237027875b3fb192280aca8081d04c32ec
Yong Yang <yangoliver@gmail.com>
2016-10-13 12:26:39 +0800

    two methods to draw an image
    Signed-off-by: Yong Yang <yangoliver@gmail.com>

---
+++ image/image.go
@@ -0,0 +1,37 @@
+// This is an implementation for https://tour.go-zh.org/methods/16
+package main
+
+import (
+	"golang.org/x/tour/pic"
+	"image"
+	"image/color"
+)
+
+type Image struct {
+	height int
+	width  int
+}
+
+func loc2color(x, y int) uint8 {
+	return uint8(x * y)
+	//return uint8((x+y) / 2)
+	//return uint8(x^y)
+}
+
+func (img Image) ColorModel() color.Model {
+	return color.RGBAModel
+}
+
+func (img Image) Bounds() image.Rectangle {
+	return image.Rect(0, 0, img.width, img.height)
+}
+
+func (img Image) At(x, y int) color.Color {
+	v := loc2color(x, y)
+	return color.RGBA{v, v, 255, 255}
+}
+
+func main() {
+	m := Image{256, 256}
+	pic.ShowImage(m)
+}
---
+++ slice/slice.go
@@ -0,0 +1,28 @@
+// Code for https://tour.go-zh.org/moretypes/15
+
+package main
+
+import "golang.org/x/tour/pic"
+
+func loc2color(x, y int) uint8 {
+	//return uint8(x * y)
+	//return uint8((x+y) / 2)
+	return uint8(x ^ y)
+}
+
+func Pic(dx, dy int) [][]uint8 {
+	img := make([][]uint8, dy)
+
+	for i := dy - 1; i >= 0; i-- {
+		img[i] = make([]uint8, dx)
+		for j := dx - 1; j >= 0; j-- {
+			img[i][j] = loc2color(i, j)
+		}
+	}
+	return img
+}
+
+func main() {
+	pic.Show(Pic)
+
+}
