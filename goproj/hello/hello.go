5d3a60538f2af28630fe4412d514ada20c749dda
Yong Yang <yangoliver@gmail.com>
2016-10-15 11:57:37 +0800

    go project exmaples for "How to Write Go Code"
    Signed-off-by: Yong Yang <yangoliver@gmail.com>

--- goproj/hello/hello.go
+++ goproj/hello/hello.go
@@ -2,8 +2,13 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/yangoliver/go-examples/goproj/stringutil"
+)
 
 func main() {
 	fmt.Printf("Hello World\n")
+
+	fmt.Printf(stringutil.Reverse("!oG ,olleH"))
 }
